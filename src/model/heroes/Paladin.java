03113cd63bd8b57712082a3fa5da83994d90b9f3
ahmedmokhtar97 <ahmedmokhtar46@gmail.com>
2020-03-08 06:56:23 +0200

    didn't build subclasses of spell and build deck

---
+++ src/model/heroes/Paladin.java
@@ -0,0 +1,15 @@
+package model.heroes;
+
+import model.cards.Card;
+import model.cards.minions.Minion;
+
+import java.io.IOException;
+import java.util.ArrayList;
+/**
+ *
+ *   A class representing a hero. No objects of type Hero can be instantiated.
+ *
+ * **/
+public class Mage extends Hero {
+    super("Uther Lightbringer");
+}
